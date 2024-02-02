db8a78b8cc628aa41064c231b1f5390a564d8ca6
Jaivin Jacob <77390216+jaivinjacob@users.noreply.github.com>
2024-02-02 13:45:27 -0500

    Update deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -2,6 +2,6 @@
 
 # Deployment script using Vagrant
 sudo apt-get update
-sudo apt-get install -y librbd1
+sudo apt-get install -y --fix-missing librbd1
 vagrant up
 vagrant ssh -c "sudo apt-get install -y python && python /vagrant/pycode.py"
