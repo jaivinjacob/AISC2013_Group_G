a67a50022eaac90a617fa56df96599dcec08d528
Jaivin Jacob <77390216+jaivinjacob@users.noreply.github.com>
2024-02-04 19:54:50 -0500

    Update deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Deployment script using Vagrant
+# Deployment script without Vagrant
 sudo apt-get update
 sudo apt-get install -y --fix-missing librbd1
+sudo apt-get install -y python
 
-# Change to the directory containing your Vagrantfile
-cd /path/to/your/vagrant/project
-
-# Check if Vagrantfile exists
-if [ -f Vagrantfile ]; then
-    vagrant up
-    vagrant ssh -c "sudo apt-get install -y python && python /vagrant/pycode.py"
-else
-    echo "Vagrantfile not found in the current directory."
-    exit 1
-fi
+# Run your Python script
+python pycode.py
